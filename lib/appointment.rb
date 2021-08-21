0d0bd64df0eadcd1cabf192274e565e07f5a0f1d
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-22 09:30:02 +1000

    Lab completed

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,18 @@
+class Appointment
+  
+  attr_accessor :name, :patient, :doctor
+  
+  @@all = []
 
+  def initialize(date, patient, doctor)
+    @name = name
+    @patient = patient
+    @doctor = doctor
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
